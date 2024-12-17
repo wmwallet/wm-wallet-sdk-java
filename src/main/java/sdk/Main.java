223cf66b5bcd4a9d430d98165f70e8a88b37abd6
wm <wm>
2024-12-17 23:38:42 +0800

    first

--- src/main/java/sdk/Main.java
+++ src/main/java/sdk/Main.java
@@ -7,8 +7,8 @@ public class Main {
         String clientKey = "resource/server/test_client.key";
         String publicKeyPath = "resource/public_key.pem";
         //todo
-        String url = "http://43.156.157.230";
-        String broker = "wm-n8admin";
+        String url = "";
+        String broker = "";
         Example example = new Example(caPath,cliCertPath,clientKey,publicKeyPath,url,broker);
         example.deposit();
     }
